b474682512249fc93c7428049a2cc0cc4e73cfe1
unknown <claudiaclrduro@gmail.com>
2025-10-16 13:27:34 +0200

    Types + App.js done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Folder from './Folder';
+import { FolderType } from './Types';
 
 function App() {
+  const [folders, setFolders] = useState([]);
+
+  const addFolder = () => {
+    const name = prompt('Nombre de la carpeta');
+    if (!name) return;
+    const newFOlder = { id: Date.now().toString(), name, moodboards: [] };
+    setFolders([...folders, newFolder]);
+  };
+
+  const updateFolder = (updateFolder) => {
+    setFolders(folders.map(f => f.id === updateFolder.id ? updateFolder : f));
+  };
+  const deleteFolder = (id) => {
+    setFolders(folders.filter(f => f.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      <header>
+        <h1>Moodboards</h1>
+        <button onClick={addFolder}>Crear carpeta</button>
       </header>
+      <div className="folders-container">
+        {folders.map(folder => (
+          <Folder
+            key={folder.id}
+            updateFolder={updateFolder}
+            deleteFolder={deleteFolder} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/Types.jsx
@@ -0,0 +1,17 @@
+export const FolderType = {
+    id: '',
+    name: '',
+    moodboards: [],
+};
+
+export const MoodboardType = {
+    id: '',
+    title: '',
+    images: [],
+};
+
+export const ImageType = {
+    id: '',
+    src: '',
+    tags: [],
+};
